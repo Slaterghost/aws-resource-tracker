dbb2999d19a93de5c01cf7c1648d52edecfab3d1
Ubuntu <ubuntu@ip-172-31-27-247.ec2.internal>
2026-04-20 19:38:58 +0000

    Added smart alert system with change detection

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -4,8 +4,23 @@
 BUCKET_NAME="aws-resource-tracker-dhruv-2026"
 TOPIC_ARN="arn:aws:sns:us-east-1:473469900971:aws-resource-alerts"
 
-# ===== REPORT FILE =====
+STATE_FILE="last_state.txt"
+CURRENT_STATE="current_state.txt"
+
 FILENAME="report_$(date +%F_%H-%M-%S).txt"
+
+# ===== GET CURRENT STATE =====
+aws ec2 describe-instances \
+--query 'Reservations[].Instances[?State.Name==`running`].InstanceId' \
+--output text > $CURRENT_STATE
+
+# ===== COMPARE WITH PREVIOUS =====
+if [ -f "$STATE_FILE" ]; then
+    diff $STATE_FILE $CURRENT_STATE > /dev/null
+    STATUS=$?
+else
+    STATUS=1
+fi
 
 # ===== GENERATE REPORT =====
 {
@@ -13,22 +28,8 @@ echo "===== AWS RESOURCE REPORT ====="
 echo "Generated on: $(date '+%Y-%m-%d %H:%M:%S')"
 echo ""
 
-echo "S3 Buckets:"
-aws s3 ls
-echo ""
-
 echo "Running EC2 Instances:"
-aws ec2 describe-instances \
---query 'Reservations[].Instances[?State.Name==`running`].InstanceId' \
---output text
-echo ""
-
-echo "Lambda Functions:"
-aws lambda list-functions --query 'Functions[].FunctionName' --output text
-echo ""
-
-echo "IAM Users:"
-aws iam list-users --query 'Users[].UserName' --output text
+cat $CURRENT_STATE
 echo ""
 
 echo "===== END OF REPORT ====="
@@ -37,10 +38,17 @@ echo "===== END OF REPORT ====="
 # ===== UPLOAD TO S3 =====
 aws s3 cp "$FILENAME" "s3://$BUCKET_NAME/"
 
-# ===== SEND ALERT (SNS) =====
-aws sns publish \
---topic-arn "$TOPIC_ARN" \
---subject "AWS Resource Report" \
---message file://"$FILENAME"
+# ===== ALERT ONLY IF CHANGE DETECTED =====
+if [ $STATUS -ne 0 ]; then
+    echo "Change detected! Sending alert..."
 
-echo "Report generated: $FILENAME"
+    aws sns publish \
+    --topic-arn "$TOPIC_ARN" \
+    --subject "AWS Alert: EC2 State Changed" \
+    --message file://"$FILENAME"
+
+    # Update state
+    cp $CURRENT_STATE $STATE_FILE
+else
+    echo "No change detected. No alert sent."
+fi
