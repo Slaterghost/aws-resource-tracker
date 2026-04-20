894e229fd6894715793447c1cbd2f33afb2abaa1
Ubuntu <ubuntu@ip-172-31-27-247.ec2.internal>
2026-04-20 14:56:11 +0000

    Final version: AWS resource tracker with S3 + SNS + SSH fix

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
-#
-###############
-#Author: Dhruv
-#Date: 19-April
-#
-#Version: v1
-#
-#This script will report the AWS resource usage
-############
-#
-set -x
 
-#AWS S3
-#AWS EC2
-#AWS LAMBDA
-#AWS IAM USERS
-#
-#list s3 buckets
-echo "print list of s3 buckets"
-aws s3 ls > resourceTracker
+# ===== CONFIG =====
+BUCKET_NAME="aws-resource-tracker-dhruv-2026"
+TOPIC_ARN="arn:aws:sns:us-east-1:473469900971:aws-resource-alerts"
 
-#list ec2 instances
-echo "print list of ec2"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+# ===== REPORT FILE =====
+FILENAME="report_$(date +%F_%H-%M-%S).txt"
 
+# ===== GENERATE REPORT =====
+{
+echo "===== AWS RESOURCE REPORT ====="
+echo "Generated on: $(date '+%Y-%m-%d %H:%M:%S')"
+echo ""
 
-#list aws lambda
-echo "print list of lambda"
-aws lambda list-functions >> resourceTracker:q!
+echo "S3 Buckets:"
+aws s3 ls
+echo ""
 
+echo "Running EC2 Instances:"
+aws ec2 describe-instances \
+--query 'Reservations[].Instances[?State.Name==`running`].InstanceId' \
+--output text
+echo ""
 
-#list iam users
-echo "print list of iam users"
-aws iam list-users
+echo "Lambda Functions:"
+aws lambda list-functions --query 'Functions[].FunctionName' --output text
+echo ""
+
+echo "IAM Users:"
+aws iam list-users --query 'Users[].UserName' --output text
+echo ""
+
+echo "===== END OF REPORT ====="
+} > "$FILENAME"
+
+# ===== UPLOAD TO S3 =====
+aws s3 cp "$FILENAME" "s3://$BUCKET_NAME/"
+
+# ===== SEND ALERT (SNS) =====
+aws sns publish \
+--topic-arn "$TOPIC_ARN" \
+--subject "AWS Resource Report" \
+--message file://"$FILENAME"
+
+echo "Report generated: $FILENAME"
